ebf356cb6d2984c8b319675d21936976503bf545
parfait18 <ahouantoparfaitsylvanus51@gmail.com>
2023-09-28 01:21:26 +0100

    fear: add algo to find duolicate in two array

--- first_assessment.js
+++ first_assessment.js
@@ -4,12 +4,27 @@ const benin = {
   capital: 'Porto-Novo',
 };
 
-const copyBenin = Object.assign({}, benin);
-copyBenin.capital = 'Cotonou';
+const copyBenin = { ...benin, capital: 'Cotonou' };
+// const copyBenin = Object.assign({}, benin);
+// copyBenin.capital = 'Cotonou';
 
 //if (benin.capital != 'Porto-novo') benin.capital = 'Porto-novo';
 
 console.log('benin', benin);
 console.log('copyBenin', copyBenin);
 
-const copyBenin = { ...benin, capital: 'Cotonou' };
+function solution(x, y) {
+  let res = [];
+  x = new Set(x);
+  x.forEach((element) => {
+    y.forEach((n) => {
+      if (element === n) {
+        res.push(element);
+      }
+    });
+  });
+
+  return res;
+}
+const result = solution([11, 12, 12, 11], [12, 12]);
+console.log(result);
