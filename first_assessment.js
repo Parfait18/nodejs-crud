53ec74531bd3c2b54d4d0abd0822a9abae1efb01
parfait18 <ahouantoparfaitsylvanus51@gmail.com>
2023-10-01 22:19:22 +0100

    update command

--- first_assessment.js
+++ first_assessment.js
@@ -28,3 +28,36 @@ function solution(x, y) {
 }
 const result = solution([11, 12, 12, 11], [12, 12]);
 console.log(result);
+
+const ops = ['5', '2', 'C', 'D', '+'];
+function getScrore(ops) {
+  var values = [];
+  var result = null;
+  var last_score = null;
+  ops.forEach((element) => {
+    if (/^[0-9]*$/.test(element)) {
+      values.push(parseInt(element));
+      last_score = parseInt(values[values.length - (values.length - 1)]);
+    } else {
+      if (element == 'D') {
+        var nw = 2 * last_score;
+        console.log(nw, 'nw');
+        values.push(nw);
+      }
+      if (element == 'C') {
+        values.pop();
+      }
+      if (element == '+') {
+        var nk = values[values.length - 1] + values[values.length - 2];
+        values.push(nk);
+      }
+    }
+  });
+  // values.forEach((item) => {
+  //   result = item + result;
+  // });
+  return values;
+}
+
+var score = getScrore(ops);
+console.log(score);
